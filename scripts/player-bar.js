91bd95458b1e29256e44e6cdcdbdc98289b3d9e0
svenlarson1098 <svenlarson1098@gmail.com>
2017-11-13 18:49:53 -0800

    Code corrected lesson complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -4,14 +4,15 @@
     $(this).attr('playState', player.playState);
   });
 
-$('button#next').on('click', function() {
-  if (player.playState !== 'playing') { return; }
+  $('button#next').on('click', function() {
+    if (player.playState !== 'playing') { return; }
 
-  const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-  const nextSongIndex = currentSongIndex + 1;
-  //if (nextSongIndex) >= album.songs.length) {return;}
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const nextSongIndex = currentSongIndex + 1;
+    if (nextSongIndex >= album.songs.length) { return; }
 
-  const nextSong = album.songs[nextSongIndex];
-  player.playPause(nextSong);
-});
+    const nextSong = album.songs[nextSongIndex];
+    player.playPause(nextSong);
+  });
+
 }
