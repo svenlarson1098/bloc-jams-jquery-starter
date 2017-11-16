5dc7ef4a29dd1bc8025ddd6b58ddb6943b4cc382
svenlarson1098 <svenlarson1098@gmail.com>
2017-11-15 22:09:05 -0800

    Helper saved progress

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,4 +1,12 @@
 class Helper {
-  helper () {
-    
+
+
+playPauseAndUpdate
 }
+const helper = new Helper();
+
+
+
+//Add a method to the Helper class called `playPauseAndUpdate` that takes one parameter, `song`.
+//The method should call `player.playPause` (passing the song parameter) and update the total time.
+//Replace every call of `player.playPause` with `helper.playPauseAndUpdate`.
