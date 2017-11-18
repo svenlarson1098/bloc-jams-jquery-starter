b21c65a71ec43a69008134e091aa13721bdec921
svenlarson1098 <svenlarson1098@gmail.com>
2017-11-17 21:48:57 -0800

    Total Time issue

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,10 +1,13 @@
 class Helper {
+  playPauseAndUpdate(song) {
+    player.playPause(song);
 
-
-playPauseAndUpdate
+    playPauseAndUpdate (song = this.currentlyPlaying) {
+      //if (this.currentlyPlaying !== song)
+      $('#total-time .duration').val(duration);
+  }
 }
 const helper = new Helper();
-
 
 
 //Add a method to the Helper class called `playPauseAndUpdate` that takes one parameter, `song`.
